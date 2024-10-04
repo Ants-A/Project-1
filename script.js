9229649c98c7d49c30a5df7454ef7a8e68ab53c6
Ants-A <ants.aas.v65@gmail.com>
2024-10-04 22:39:02 +0300

    Design changes
    Lisasin uuenduse ajad, kasti kuhu panna kui suurt summat saab muuta ja mõni värvimuutus

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let convert_start;
 let convert_end;
+let money;
+let end_amount;
+const url = "https://open.er-api.com/v6/latest/USD";
 
 const currency_start = document.querySelector(".currency_start");
 currency_start.addEventListener('keydown', function(event) 
@@ -7,7 +10,6 @@ currency_start.addEventListener('keydown', function(event)
     if (event.key == "Enter")
     {
         convert_start = event.target.value;
-        console.log(convert_start) 
         download()
     }
 });
@@ -18,30 +20,51 @@ currency_end.addEventListener('keydown', function(event)
     if (event.key == "Enter")
     {
         convert_end = event.target.value;
-        console.log(convert_end)   
         download()
     }
+});
+
+const starting_amount = document.querySelector(".amount");
+starting_amount.addEventListener("keydown", function(event)
+{
+    if (event.key == "Enter")
+        {
+            money = event.target.value;
+            download()
+        }
 });
 
 
 async function download() 
 {
-    const url = "https://open.er-api.com/v6/latest/USD";
     const response = await fetch(url);
     const data = await response.json();
     const element = document.querySelector(".currency_conv");
-    let converted;
-    console.log(convert_end)  
-    console.log(convert_start) 
-    if (data.rates[convert_end] == 1)
-    {
-        converted = data.rates[convert_end] / data.rates[convert_start];
-    }
-    else
-    {
-        converted = data.rates[convert_start] / data.rates[convert_end];
-    }
-    console.log(data.rates[convert_start])
 
-    element.innerHTML = "<p>" + data.rates[convert_start] + " ==> " + converted + "</p>";
+    const converted = data.rates[convert_end] / data.rates[convert_start];
+    const conv_money = money * converted;
+    
+    if (convert_start != undefined && convert_end != undefined
+       && money != undefined && conv_money != undefined  
+    ){
+        element.innerHTML = 
+        "<p>" 
+        + money + " " + convert_start + " ==> " 
+        + conv_money + " " + convert_end
+        "</p>";
+    }   
 }
+
+async function dates() 
+{
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const last_upt = document.querySelector(".last_upt");
+    last_upt.innerHTML += " " + data.time_last_update_utc;
+
+    const next_upt = document.querySelector(".next_upt");
+    next_upt.innerHTML += " " + data.time_next_update_utc
+}
+
+dates()
